0fb353f9c67f24336879bf3d4b07ae992c3750cd
federlian <stepanfederlan@gmail.com>
2020-08-25 23:46:21 +0300

    vue hw 1

--- vue.js/hw1/src/App.vue
+++ vue.js/hw1/src/App.vue
@@ -1,28 +1,56 @@
+<!--Створити простий todolist В папці assests є скрін як приблизно має виглядати Має бути масив тудушок, 
+в інпуті вписуємо туду і пушимо в масив за допомогою кнопки Add. 
+Видаляємо тудушки за допомогою кнопки delete. Якщо тудушок більше 10 кнопка повинна сховатись.-->
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <input type="text" v-model="text" v-on:keyup.enter="showText()">
+    <button @click="showText()" v-if="show">Add</button>
+    <ul>
+      <li v-for="(mass, index) in arr" :key="index">
+        <p>{{ mass }}<button @click="deleteText(index)">Delete</button></p>
+      </li>
+    </ul>
+
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      text: null,
+      arr: [],
+      show: true
+    }
+
+  },
+
+  methods: {
+    showText() {
+      if (this.arr.length < 10) {
+        this.arr.push(this.text);
+      } else {
+        this.show = false;
+
+      }
+    },
+
+    deleteText(index) {
+      this.arr.splice(index, 1);
+      this.show = true;
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
