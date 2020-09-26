ed158f0f14ac1be56511928da3fd0d5ef6ec483e
federlian <stepanfederlan@gmail.com>
2020-09-27 00:27:24 +0300

    complete hw1

--- angular/hw1/src/app/app.component.ts
+++ angular/hw1/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {Todo} from "./todo";
+import {TodoDataService} from "./todo-data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'hw1';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
